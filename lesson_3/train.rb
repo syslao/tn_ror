62dd111b01399897b61f8d5fd9cff5cba75c1906
syslao <syslao@gmail.com>
2017-05-25 18:25:06 +0300

    add many methods

--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -1,10 +1,12 @@
 class Station
   attr_accessor :trains_on_station
+  attr_reader :name
 
-  def initialize(name, trains_on_station = [])
+  def initialize(name)
     @name = name
-    @trains_on_station = trains_on_station
   end
+
+  @trains_on_station = []
 
   def add_train(train)
     @trains_on_station << train
@@ -15,7 +17,8 @@ class Station
   end
 
   def trains
-    @trains_on_station
+    puts "#{@trains_on_station.length} train/s on station"
+    @trains_on_station.each { |train| puts "Number #{train.number}" }
   end
 
   def trains_type
@@ -26,45 +29,81 @@ end
 
 class Route
   def initialize(first_station, last_station)
-    @first_station = first_station
-    @last_station = last_station
+    @route_list = [first_station, last_station]
   end
 
-  def add_station
+  def add_station(station)
+    @route_list.include?(station) ? (puts 'already in route list') : @route_list.insert(-2, station)
   end
 
-  def del_station
+  def del_station(station)
+    @route_list.delete(station)
   end
 
   def stations_list
+    @route_list.each { |station| puts station.name }
   end
 end
 
 class Train
-  attr_accessor :speed
-  attr_reader :quantity
-  attr_reader :type
+  attr_reader :number, :quantity, :type
+  attr_accessor :speed, :current_station, :route_list
 
   def initialize(number, type, quantity, speed = 0)
     @number = number
     @type = type.to_sym
     @quantity = quantity
     @speed = speed
+    @current_station = {}
   end
 
   def stop
     @speed = 0
   end
 
-  def something(arg)
+  def current_speed
+    puts @speed
   end
 
-  def current_statin
+  def wagons_quantity
+    puts @quantity
   end
 
-  def prev_statin
+  def wagons(arg)
+    return puts 'Train in motion' if @speed > 0
+    if arg == 'add'
+      @quantity += 1
+    elsif arg == 'remove'
+      @quantity > 0 ? @quantity -= 1 : (puts 'There are no more wagons to remove')
+    end
   end
 
-  def next_statin
+  def add_route(route)
+    @route_list = route.stations_list
+    @current_station = @route_list[0]
+  end
+
+  def next
+    if @route_list.index(@current_station) == @route_list.length - 1
+      puts "its last station"
+    else
+      index_next_station = @route_list.index(@current_station) + 1
+      @current_station = @route_list[index_next_station]
+  end
+  end
+
+  def prev
+    if @route_list.index(@current_station).zero?
+      puts 'its first station'
+    else
+      index_prev_station = @route_list.index(@current_station) - 1
+      @current_station = @route_list[index_prev_station]
+    end
+  end
+
+  def next_station
+  end
+
+  def prev_station
   end
 end
