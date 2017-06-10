99f33ae44f971e7f1ced911abede5886ee9d61bb
modems <syslao@gmail.com>
2017-06-10 12:48:13 +0300

    add methods and validation for cargo and passanger wagon class

--- lesson_7/cargo_wagon.rb
+++ lesson_7/cargo_wagon.rb
@@ -1,5 +1,26 @@
 class CargoWagon < Wagon
-  def initialize
+  include Validate
+  attr_reader :payload, :taked_payload
+  def initialize(payload)
     @type = :cargo
+    @payload = payload
+    @taked_payload = 0
+    validate!
+  end
+
+  def take_payload(value)
+    @taked_payload += value
+  end
+
+  def free_payload
+    @payload - @taked_payload
+  end
+
+  private
+
+  def validate!
+    raise 'Value cant be nil' if payload.nil?
+    raise 'Value should be more than 0' if payload < 1
+    true
   end
 end
--- lesson_7/passenger_wagon.rb
+++ lesson_7/passenger_wagon.rb
@@ -1,5 +1,26 @@
 class PassengerWagon < Wagon
-  def initialize
+  include Validate
+  attr_reader :seats, :booked_seats
+  def initialize(seats)
     @type = :passenger
+    @seats = seats
+    @booked_seats = 0
+    validate!
+  end
+
+  def take_seat
+    @booked_seats += 1
+  end
+
+  def free_seats
+    @seats - @booked_seats
+  end
+
+  private
+
+  def validate!
+    raise 'Seats cant be nil' if seats.nil?
+    raise 'Seats should be more than 0' if seats < 1
+    true
   end
 end
