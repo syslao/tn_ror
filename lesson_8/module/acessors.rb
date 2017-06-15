c044e678032a9330fcfac7780ec49302e2c40715
syslao <syslao@gmail.com>
2017-06-16 00:39:11 +0300

    add attr_accessor_with_history method

--- lesson_8/module/acessors.rb
+++ lesson_8/module/acessors.rb
@@ -1,2 +1,29 @@
 module Acessors
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def attr_accessor_with_history(*names)
+      names.each do |name|
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) do |value| 
+        instance_variable_set(var_name, value)
+        attr = (var_name.to_s + "_history").to_sym
+        attr_history = instance_variable_get(attr) || []
+        instance_variable_set(attr, attr_history.push(value)) 
+      end
+      end
+    end
+
+    def strong_attr_acessor
+    end
+  end
+
+  module InstanceMethods
+    def some_history
+    end
+  end
 end
