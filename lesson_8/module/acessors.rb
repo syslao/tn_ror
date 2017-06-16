72e022c1a949b557a6440a3d21012c3f36754c06
syslao <syslao@gmail.com>
2017-06-16 14:36:44 +0300

    add *attr*_history method and some fix

--- lesson_8/module/acessors.rb
+++ lesson_8/module/acessors.rb
@@ -1,30 +1,26 @@
 module Acessors
-  def self.included(base)
-    base.extend ClassMethods
-    base.send :include, InstanceMethods
-  end
-
-  module ClassMethods
-    def attr_accessor_with_history(*names)
-      names.each do |name|
-      var_name = "@#{name}".to_sym
-      attr = (var_name.to_s + "_history").to_sym
-      attr_history = instance_variable_get(attr) || []
-      define_method((name + "_history").to_sym) { instance_variable_get(attr)  }
-      define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}=".to_sym) do |value| 
-        instance_variable_set(var_name, value)
-        instance_variable_set(attr, attr_history.push(value))
-      end
-      end
+  def attr_accessor_with_history(*names)
+    names.each do |name|
+    var_name = "@#{name}".to_sym
+    attr = (var_name.to_s + "_history").to_sym
+    attr_history = instance_variable_get(attr) || []
+    define_method((name + "_history").to_sym) { instance_variable_get(attr)  }
+    define_method(name) { instance_variable_get(var_name) }
+    define_method("#{name}=".to_sym) do |value| 
+      instance_variable_set(var_name, value)
+      instance_variable_set(attr, attr_history.push(value))
     end
-
-    def strong_attr_acessor(name, class)
     end
   end
 
-  module InstanceMethods
-    def some_history
+  def strong_attr_acessor(name, cls)
+    var_name = "@#{name}".to_sym
+    attr_class = Module.const_get(cls)
+    define_method(name) { instance_variable_get(var_name) }
+    define_method("#{name}=".to_sym) do |value|
+      instance_variable_set("@#{name}_class".to_sym, attr_class)
+      raise "Object Class and given Class not equal" if value.class != attr_class
+      instance_variable_set(var_name, value) 
     end
   end
 end
